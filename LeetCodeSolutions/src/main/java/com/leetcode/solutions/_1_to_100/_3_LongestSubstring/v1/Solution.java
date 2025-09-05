90647f948d7aa3d6855c8c8d95512781da81f339
prosenjit005 <nanobyte.academy.tech@gmail.com>
2025-09-05 09:32:53 +0530

    Create Solution.java

---
+++ LeetCodeSolutions/src/main/java/com/leetcode/solutions/_1_to_100/_3_LongestSubstring/v1/Solution.java
@@ -0,0 +1,32 @@
+package com.leetcode.solutions._1_to_100._3_LongestSubstring.v1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> charSet = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (charSet.contains(s.charAt(right))) {
+                charSet.remove(s.charAt(left));
+                left++;
+            }
+
+            charSet.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String input = "pwwkew";
+        int output = solution.lengthOfLongestSubstring(input);
+        System.out.println("input: " + input);
+        System.out.println("output: " + output);
+    }
+}
